2240ecbea6299990ea12ddbf13357d80a62efbae
Chris Bowen <cbowen216@gmail.com>
2022-07-24 11:54:33 -0700

    user model

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from users.models import Users
 
 
-class AritistSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ('id', 'email', 'first_name', 'last_name', 'password')
