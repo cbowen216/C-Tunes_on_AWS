64af5f87f37509f29bb1e314064bc59ff8f9cb5e
Chris Bowen <cbowen216@gmail.com>
2022-07-29 13:58:03 -0700

    troubleshooting

--- app/users/urls.py
+++ app/users/urls.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('register/', user_views.register, name='register'),
-    path('login/',
+    path('login',
          auth_views.LoginView.as_view(template_name='users/login.html'),
          name='login'),
     path('logout/',
