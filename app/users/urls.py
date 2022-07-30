3fe73e492cebd5d08bc2b4a7b6cb47c8b902a86f
Chris Bowen <cbowen216@gmail.com>
2022-07-30 12:00:05 -0700

    troubleshooting

--- app/users/urls.py
+++ app/users/urls.py
@@ -7,11 +7,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('register/', user_views.register, name='register'),
-    path('login/',
-         auth_views.LoginView.as_view(template_name='users/login.html'),
-         name='login'),
-    path('logout/',
-         auth_views.LogoutView.as_view(template_name='users/logout.html'),
-         name='logout'),
+     path('api/users/', user_views.user_list),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+'''
+     path('register/', user_views.register, name='register'),
+     path('login/',
+          auth_views.LoginView.as_view(template_name='users/login.html'),
+          name='login'),
+     path('logout/',
+          auth_views.LogoutView.as_view(template_name='users/logout.html'),
+          name='logout'),
+'''
