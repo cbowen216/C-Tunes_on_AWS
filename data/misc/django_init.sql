9a3ce8a50cd517cb19debcc2032ef9aea5e22ff8
Chris Bowen <cbowen216@gmail.com>
2022-07-30 11:28:28 -0700

    first seed data

--- data/misc/django_init.sql
+++ data/misc/django_init.sql
@@ -11,3 +11,32 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET default_tablespace = '';
 SET default_with_oids = false;
+
+-- reset tables (erase data)
+TRUNCATE TABLE artists CASCADE;
+TRUNCATE TABLE users CASCADE;
+
+-- add some data 
+
+-- Users
+INSERT INTO users (email, password,  first_name,  last_name)
+VALUES ('chris@chris.com', 'password123', 'Chris', 'Bowen');
+
+INSERT INTO users (email, password,  first_name,  last_name)
+VALUES ('you@domain.com', 'password', 'Andy', 'lastname');
+
+INSERT INTO users (email, password,  first_name,  last_name)
+VALUES ('other@place.com', 'mypassword', 'Chris', 'Jones');
+
+-- artists
+INSERT INTO artists (name)
+VALUES ('Metallica');
+
+INSERT INTO artists (name)
+VALUES('Pink Floyd');
+
+INSERT INTO artists (name)
+VALUES('Beatles');
+
+INSERT INTO artists (name)
+VALUES('Missy Elliot');
